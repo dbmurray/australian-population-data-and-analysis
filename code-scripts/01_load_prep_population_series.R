b9a9fb46692d32126a426422533b043b41aa71e7
Darragh Murray <darragh.murray@gmail.com>
2022-12-04 08:33:13 +1000

    Updated data preparation code and tables

--- code-scripts/01_load_prep_population_series.R
+++ code-scripts/01_load_prep_population_series.R
@@ -2,15 +2,15 @@
 # for further downstream anaylsis of migration statistics. 
 
 # LIBRARIES
-
 library(readabs)
 library(dplyr)
 
 # IMPORT DATA
 
-# import the data and seperate out the series
+# import the data and separate out the series
 australian_demographic_statistics <- read_abs("3101.0") %>%
-  separate_series() 
+  separate_series() %>%
+  mutate(date = as.Date(date))
 
 
 # TIDY DATA
@@ -18,14 +18,13 @@ australian_demographic_statistics <- read_abs("3101.0") %>%
 # filter for the appropriate data series based on location (national, states) and select the appropriate metrics.
 
 australian_migration_data <- australian_demographic_statistics %>%
-  filter(ifelse(series_3 =="Australia",
-                series_2 == c("Net Overseas Migration", "Overseas Arrivals", "Overseas Departures"), 
-                series_2 == c("Interstate Arrivals", "Interstate Departures", 
-                              "Net Interstate Migration", "Net Overseas Migration"))) %>% # filtering for relevant series
   rename(location=series_3,metric = series_2, metric_value=value) %>% # better variable names.
+  filter(metric == "Interstate Arrivals" | metric == "Interstate Departures" | metric == "Net Interstate Migration",
+         location != "Australia") %>% # filter for required variables
   mutate(migration_category_series = if_else(metric == "Net Overseas Migration", 
-                                             "Overseas Migration","Interstate Migration")) %>% 
-  select(migration_category_series, date, location, metric, metric_value)
+                                             "Overseas","Interstate"),) %>% 
+  select(migration_category_series, date, location, metric, metric_value) # select needed fields only
 
 # OUTPUT
+
 write.csv(australian_migration_data, "output/tables/australia_migration_data.csv", row.names = FALSE)
