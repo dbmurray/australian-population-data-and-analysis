88d4eeac5293427c7f5519638f22bf1b84f266f0
Darragh Murray <darragh.murray@gmail.com>
2022-12-03 20:43:45 +1000

    Initial Script

--- code-scripts/01_load_prep_population_series.R
+++ code-scripts/01_load_prep_population_series.R
@@ -1,5 +1,5 @@
 # This script downloads Australian Demographic Statistics in R and prepares them
-# for further downstream anaylsis. 
+# for further downstream anaylsis of migration statistics. 
 
 # LIBRARIES
 
@@ -14,15 +14,18 @@ australian_demographic_statistics <- read_abs("3101.0") %>%
 
 
 # TIDY DATA
+
+# filter for the appropriate data series based on location (national, states) and select the appropriate metrics.
+
 australian_migration_data <- australian_demographic_statistics %>%
   filter(ifelse(series_3 =="Australia",
                 series_2 == c("Net Overseas Migration", "Overseas Arrivals", "Overseas Departures"), 
-                series_2 == c("Interstate Arrivals", "Interstate Departures", "Net Interstate Migration", "Net Overseas Migration"))) %>%
-  rename(location=series_3,metric = series_2, metric_value=value) %>%
-  mutate(migration_category_series = if_else(metric == "Net Overseas Migration", "Overseas Migration","Interstate Migration")) %>%
+                series_2 == c("Interstate Arrivals", "Interstate Departures", 
+                              "Net Interstate Migration", "Net Overseas Migration"))) %>% # filtering for relevant series
+  rename(location=series_3,metric = series_2, metric_value=value) %>% # better variable names.
+  mutate(migration_category_series = if_else(metric == "Net Overseas Migration", 
+                                             "Overseas Migration","Interstate Migration")) %>% 
   select(migration_category_series, date, location, metric, metric_value)
-
-
 
 # OUTPUT
 write.csv(australian_migration_data, "output/tables/australia_migration_data.csv", row.names = FALSE)
